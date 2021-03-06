f2b97ed409377066990a1da8c3550a8e4cbd5212
Jacob Simonek <78231728+Simoja0428@users.noreply.github.com>
2021-03-06 11:14:58 -0600

    Create dataTypes.cpp

---
+++ dataTypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  int wholeNum = 1;
+  double decimalNum = 3.25;
+  bool flag = True;
+  char letter = 'a';
+  
+  cout << "The whole number value is: " << wholeNum << endl;
+  cout << "The decimal number value is: " << decimalNum << endl;
+  cout << "The value of the flag is: " << flag << endl;
+  cout << "The character value is: " << letter << endl;
+  
+  return 0;
+}
